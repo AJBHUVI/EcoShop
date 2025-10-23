e99cf08b31ee7e88c5fa39259f1ba77e8ded73dd
AJBHUVI <ajbhuvi18@gmail.com>
2025-10-23 16:22:14 +0530

    new

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,14 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'mysql_db', // Use Docker service name
+  host: process.env.DB_HOST || 'localhost', // Use Docker service name
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'rootpass',
   database: process.env.DB_NAME || 'ecoshop',
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  port: process.env.DB_PORT || 3306
 });
 
 export default pool;
